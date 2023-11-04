57d6f48415553fc744836a87ed5a727a114d6552
gabrielaguiardantas <gabrielaguiardantas@gmail.com>
2023-11-04 11:40:47 -0300

    v1 completo

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './redux/index.ts'
-import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./redux/index.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
