fe56a5364dfa07e012b2ac724ff13a06aba76239
gabrielaguiardantas <gabrielaguiardantas@gmail.com>
2023-11-04 13:35:35 -0300

    improve detection width on mount

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,19 @@ import Header from "./components/header";
 import NavBar from "./components/nav-bar";
 import Footer from "./components/footer";
 import Main from "./components/main";
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 
 function App() {
-  const [viewPort, setViewPort] = useState("desktop"); // desktop | mobile
+  const [viewPort, setViewPort] = useState(""); // desktop | mobile
 
+  useLayoutEffect(() => {
+    if (window.innerWidth <= 1024) {
+      setViewPort("mobile");
+    } else {
+      setViewPort("desktop");
+    }
+  }, []);
+  
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth <= 1024) {
@@ -20,7 +28,8 @@ function App() {
     return () => {
       window.removeEventListener("resize", handleResize);
     };
-  })
+  });
+
   return (
     <div className={ viewPort === "mobile" ? "mobile" : "desktop"}>
       <Header />
