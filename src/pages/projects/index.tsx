97d5d16556ebd470f0ffad8fcaed2de00390222c
gabrielaguiardantas <gabrielaguiardantas@gmail.com>
2023-11-04 12:05:14 -0300

    v1 completo

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,9 +1,8 @@
 import { useSelector } from "react-redux";
-import { RootState } from "../../types";
-import { githubPages } from "../stacks/centralIconsFile";
+import { RootState } from "../../types/index.ts";
+import { githubPages } from "../stacks/centralIconsFile.ts";
 import "./projects.css";
 import { projectsArray } from "./projectsData.ts";
-
 
 function Projects() {
   const language_page = useSelector((state: RootState) => state.language);
@@ -25,10 +24,22 @@ function Projects() {
       <table cellSpacing="15">
         <thead>
           <tr>
-            <th>{ language_page === "portuguese" ? <>Ano</> : <>Year</>}</th>
-            <th>{ language_page === "portuguese" ? <>Nome</> : <>Name</>}</th>
-            <th>{ language_page === "portuguese" ? <>Descrição</> : <>Description</>}</th>
-            <th>{ language_page === "portuguese" ? <>Principais Stacks</> : <>Built with</>}</th>
+            <th>{language_page === "portuguese" ? <>Ano</> : <>Year</>}</th>
+            <th>{language_page === "portuguese" ? <>Nome</> : <>Name</>}</th>
+            <th>
+              {language_page === "portuguese" ? (
+                <>Descrição</>
+              ) : (
+                <>Description</>
+              )}
+            </th>
+            <th>
+              {language_page === "portuguese" ? (
+                <>Principais Stacks</>
+              ) : (
+                <>Built with</>
+              )}
+            </th>
             <th>Link</th>
           </tr>
         </thead>
