dbc140326c63ecbbd79b224f253fedf0646d67d9
gabrielaguiardantas <gabrielaguiardantas@gmail.com>
2023-11-04 11:10:51 -0300

    v1 completo

--- personal_portfolio/src/redux/actions/index.ts
+++
@@ -1,4 +0,0 @@
-export const actionCreator = (changeLanguage: string) => ({ 
-    type: 'SWITCH_LANGUAGE',
-    payload: changeLanguage,
-  });
--- personal_portfolio/src/redux/index.ts
+++
@@ -1,7 +0,0 @@
-import { legacy_createStore as createStore } from "redux";
-import { composeWithDevTools } from "@redux-devtools/extension";
-import languageReducer from "./reducers/languageReducer";
-
-const store = createStore(languageReducer, composeWithDevTools());
-
-export default store;
